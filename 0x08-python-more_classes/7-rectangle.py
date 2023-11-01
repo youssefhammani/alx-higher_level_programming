ec1ebc6a698ed417f3471ac198f84a2e261f8a6f
youssefhammani <youssefhammani04@gmail.com>
2023-11-01 12:30:23 -0700

    Change representation

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" Define a Rectangle Class """
+
+
+class Rectangle:
+    """ define Regtangle class """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return (((str(self.print_symbol) * self.width)+"\n") *
+                self.height)[:-1]
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
