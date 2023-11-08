26e1d6be30824ae7d96d88985a0ae5cd65c6b691
youssefhammani <youssefhammani04@gmail.com>
2023-11-08 13:27:49 -0800

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" define student class """
+
+
+class Student:
+    """ define student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ convert to json """
+        if attrs is None:
+            return vars(self)
+
+        serializable_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                serializable_dict[attr] = getattr(self, attr)
+        return serializable_dict
+
+    def reload_from_json(self, json):
+        """ replaces all attributes """
+        for key, value in json.items():
+            setattr(self, key, value)
