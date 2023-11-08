353a0824b2fa2480b9ab28dd23695d39765516fb
youssefhammani <youssefhammani04@gmail.com>
2023-11-08 13:57:55 -0800

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Function to return attributes and methods of an object """
+
+
+def lookup(obj):
+    """ Function to return attributes and methods of an object """
+    return dir(obj)
