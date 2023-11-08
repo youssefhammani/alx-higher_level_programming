78e39238e3efc532cb23e50a31af3dfeffd3aa45
youssefhammani <youssefhammani04@gmail.com>
2023-11-08 13:16:33 -0800

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""  function that returns the dictionary description in Json """
+
+
+def class_to_json(obj):
+    """  function that returns the dictionary description in Json """
+    return vars(obj)
