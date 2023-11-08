51a7ccc8fd3684a98b6a7f0377bcbb52c2f65249
youssefhammani <youssefhammani04@gmail.com>
2023-11-08 07:35:00 -0800

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ and then save them to a file
 
 import json
 import sys
-import os
+import os.path
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
