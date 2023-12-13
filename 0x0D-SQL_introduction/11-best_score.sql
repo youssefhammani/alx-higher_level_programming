1a4b9c42048711df3cff6ef1eb6dd4279e6ae282
youssefhammani <youssefhammani04@gmail.com>
2023-12-12 16:33:00 -0800

    Select the best

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- List records with a score >= 10 in the table second_table ordered by score (top first)
 SELECT score, name
-FROM hbtn_0c_0.second_table
+FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;
