9439b18701e73ac32b8d16aae401789f27f663ed
youssefhammani <youssefhammani04@gmail.com>
2023-12-13 15:58:23 -0800

    Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,6 @@
 -- List cities of California without using JOIN
 SELECT * FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id;
+WHERE state_id = (
+	SELECT id FROM states
+	WHERE name = "California"
+) ORDER BY id;
