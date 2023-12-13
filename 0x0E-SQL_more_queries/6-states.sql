d21c96ee28fae6a388a609b4d084a85a0091c940
youssefhammani <youssefhammani04@gmail.com>
2023-12-13 15:41:50 -0800

    States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Create database hbtn_0d_usa if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Create table states
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
