c97e0e7b034be0ab5324ed5bde5b3b8367014f3d
youssefhammani <youssefhammani04@gmail.com>
2023-12-14 12:29:28 -0800

    Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,13 +1,8 @@
 -- List all genres not linked to the show Dexter
-SELECT
-    tv_genres.name
-FROM
-    tv_genres
-LEFT JOIN
-    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN
-    tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE
-    tv_shows.title != 'Dexter' OR tv_shows.title IS NULL
-ORDER BY
-    tv_genres.name ASC;
+SELECT tv_genres.name FROM tv_genres
+WHERE tv_genres.id NOT IN (
+      SELECT tv_genres.id FROM tv_genres
+      JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+      JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+      WHERE tv_shows.title = "DEXTER" )
+ORDER BY tv_genres.name ASC;
