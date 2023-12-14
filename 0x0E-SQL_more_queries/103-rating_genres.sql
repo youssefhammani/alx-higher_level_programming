e65dbde2a5aff46537ee0858f7293a50f0b63afe
youssefhammani <youssefhammani04@gmail.com>
2023-12-14 12:38:42 -0800

    Best genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,14 @@
+-- List all genres by their rating sum in descending order
+SELECT
+    tv_genres.name,
+    SUM(tv_show_ratings.rate) AS rating
+FROM
+    tv_genres
+LEFT JOIN
+    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN
+    tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY
+    tv_genres.id, tv_genres.name
+ORDER BY
+    rating DESC;
