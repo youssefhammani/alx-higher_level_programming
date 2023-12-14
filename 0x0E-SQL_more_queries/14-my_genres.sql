37b8c0298606c364a523336fe2a05041a51a0bd3
youssefhammani <youssefhammani04@gmail.com>
2023-12-14 12:01:52 -0800

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- Select the genre names for the show Dexter Filter to show only Dexter Order the results by genre name in ascending order
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
