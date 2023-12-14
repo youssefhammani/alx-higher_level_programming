08b4a527a7f9109c6a2376debcce22c58d85bd55
youssefhammani <youssefhammani04@gmail.com>
2023-12-14 12:32:30 -0800

    No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,11 +3,16 @@ SELECT
     tv_shows.title
 FROM
     tv_shows
-LEFT JOIN
-    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN
-    tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE
-    tv_genres.name != 'Comedy' OR tv_genres.name IS NULL
+    tv_shows.id NOT IN (
+        SELECT
+            tv_show_genres.show_id
+        FROM
+            tv_show_genres
+        JOIN
+            tv_genres ON tv_show_genres.genre_id = tv_genres.id
+        WHERE
+            tv_genres.name = 'Comedy'
+    )
 ORDER BY
     tv_shows.title ASC;
