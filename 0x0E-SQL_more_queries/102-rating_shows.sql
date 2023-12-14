dc0dd5d11cc5186ca5e20235afa5fc75d5ef8f2c
youssefhammani <youssefhammani04@gmail.com>
2023-12-14 12:33:56 -0800

    -- Use the hbtn_0d_tvshows_rate database

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,12 @@
+-- List all shows by their rating sum in descending order
+SELECT
+    tv_shows.title,
+    SUM(tv_show_ratings.rate) AS rating
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY
+    tv_shows.id, tv_shows.title
+ORDER BY
+    rating DESC;
