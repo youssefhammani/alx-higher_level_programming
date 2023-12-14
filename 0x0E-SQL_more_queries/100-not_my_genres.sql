cc5b3ee62cb98fd1b389b89d466d84aafe8e40ee
youssefhammani <youssefhammani04@gmail.com>
2023-12-14 12:23:40 -0800

    Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,7 +6,7 @@ FROM
 LEFT JOIN
     tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN
-    tv_shows ON tv_show_genres.show_id = tv_shows.id AND tv_shows.title = 'Dexter'
+    tv_shows ON tv_show_genres.show_id = tv_shows.id AND tv_shows.title = 'DEXTER'
 WHERE
     tv_shows.id IS NULL
 ORDER BY
