dc9b7a6701ae4f67d930d594501e0bf6bf08b299
youssefhammani <youssefhammani04@gmail.com>
2023-12-14 11:50:29 -0800

    Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Count shows by genre and display the result
-SELECT name AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres JOIN tv_show_genres ON tv_show_genres.genres_id = tv
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
