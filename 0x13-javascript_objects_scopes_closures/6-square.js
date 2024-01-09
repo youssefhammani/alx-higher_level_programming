375190a2424b6036fc94420c3bcc3f601e90a036
youssefhammani <youssefhammani04@gmail.com>
2024-01-09 22:50:00 +0100

    Rectangle #6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,19 +3,16 @@
 const SquareParent = require('./5-square');
 
 class Square extends SquareParent {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-	    let s = '';
-	    for (let j = 0; j < this.width; j++) {
-		    s +=c;
-	    }
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
     }
   }
 }
