a62ed4639512bb8fe5a3dfc8b2933329d7daf81c
youssefhammani <youssefhammani04@gmail.com>
2024-01-09 23:01:41 +0100

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-
 const list = require('./100-data.js').list;
-
 console.log(list);
-console.log(list.map((value, index) => value * index));
+console.log(list.map((item, index) => item * index));
