9108ab9c2067d07e70b22f2d2943fbe76875d72c
youssefhammani <youssefhammani04@gmail.com>
2024-01-09 22:48:41 +0100

    Rectangle #6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -12,7 +12,10 @@ class Square extends SquareParent {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+	    let s = '';
+	    for (let j = 0; j < this.width; j++) {
+		    s +=c;
+	    }
     }
   }
 }
