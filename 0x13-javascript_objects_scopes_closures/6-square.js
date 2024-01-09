2032d1fcf4a1158fd386ae8ad6fee48db5ef821a
youssefhammani <youssefhammani04@gmail.com>
2024-01-09 22:45:05 +0100

    Rectangle #6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,12 +8,10 @@ class Square extends SquareParent {
   }
 
   charPrint (c) {
-    // If c is undefined, use the character X
     if (c === undefined) {
       c = 'X';
     }
 
-    // Print the square using the specified character
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
