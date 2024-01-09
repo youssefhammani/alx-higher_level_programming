f7760fc148b1c1ef52ef4de617f120259aa45774
youssefhammani <youssefhammani04@gmail.com>
2024-01-09 22:51:55 +0100

    Rectangle #7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  const occurrences = list.filter(element => element === searchElement);
+
+  return occurrences.length;
+};
