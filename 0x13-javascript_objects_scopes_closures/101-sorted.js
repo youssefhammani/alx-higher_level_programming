541b890b05bccc639e8f8cd7250433a79fd5175a
youssefhammani <youssefhammani04@gmail.com>
2024-01-09 23:04:48 +0100

    Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const { dict } = require('./101-data');
+
+const sortedDict = {};
+for (const userId in dict) {
+  const occurrences = dict[userId];
+  
+  if (!sortedDict[occurrences]) {
+    sortedDict[occurrences] = [];
+  }
+  
+  sortedDict[occurrences].push(userId.toString());
+}
+
+console.log(sortedDict);
