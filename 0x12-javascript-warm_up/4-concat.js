862c3c9c4c51da902af6f4419bf41a3679d908cd
youssefhammani <youssefhammani04@gmail.com>
2024-01-13 15:45:30 +0100

    script that prints two arguments passed to it, in the following format: “ is ”

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Check if at least two arguments are passed and print them in the specified format
+if (process.argv[2] && process.argv[3]) {
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
+} else {
+  console.log('undefined is undefined');
+}
