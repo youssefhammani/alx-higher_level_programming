fb7df7782c45c03e576705ecba7926deb4f7f64f
youssefhammani <youssefhammani04@gmail.com>
2024-01-13 11:16:43 -0800

    City relationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,7 +5,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from model_state import Base
+from relationship_state import Base
 
 
 class City(Base):
