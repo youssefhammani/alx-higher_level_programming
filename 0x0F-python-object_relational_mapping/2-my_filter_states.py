9daf92cf24bf3d9bb574d2efb68120843ab48fda
youssefhammani <youssefhammani04@gmail.com>
2024-01-13 09:18:01 -0800

    script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,9 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Use format to create the SQL query with user input
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(state_name)
+    query = "SELECT * FROM states\
+            WHERE name = '{}'\
+            ORDER BY id ASC;".format(state_name)
     cursor.execute(query)
 
     # Fetch all the rows and display the results
