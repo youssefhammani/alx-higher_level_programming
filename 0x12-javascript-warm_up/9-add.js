749ba0ec3c39ada414af12af1bf6b2d574ed3962
youssefhammani <youssefhammani04@gmail.com>
2024-01-13 16:11:01 +0100

    script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Function to add two integers
+function add(a, b) {
+  return parseInt(a) + parseInt(b);
+}
+
+// Check if both arguments are provided
+if (process.argv[2] && process.argv[3]) {
+  // Call the add function and use console.log(...) to print the result
+  console.log(add(process.argv[2], process.argv[3]));
+} else {
+  // Print an error message if any argument is missing
+  console.log("NaN");
+}
