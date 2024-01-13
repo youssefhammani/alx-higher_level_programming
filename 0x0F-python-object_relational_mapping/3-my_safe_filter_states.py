5bbed7d0a9ceba03203e16d5679dd3868a25e3d1
youssefhammani <youssefhammani04@gmail.com>
2024-01-13 09:39:32 -0800

    script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, that is safe from MySQL injections!

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument (safe from MySQL injection)
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> \
+                <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Connection parameters
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server at localhost:3306
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database, charset="utf8")
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Use parameterized query to prevent SQL injection
+    query = "SELECT * FROM states\
+             WHERE states.name = %s\
+             ORDER BY states.id ASC;"
+    cursor.execute(query, (state_name,))
+
+    # Fetch all the rows and display the results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
