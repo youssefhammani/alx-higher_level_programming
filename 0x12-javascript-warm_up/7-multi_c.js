be0fb1d756dee119ed06f10fec513c6b3b72d915
youssefhammani <youssefhammani04@gmail.com>
2024-01-13 16:00:26 +0100

    script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,16 +2,10 @@
 // prints 3 lines: (like 1-multi_languages.js) but by
 // using an array of string and a loop
 
-if (process.argv[2]) {
-  const numOccurrences = parseInt(process.argv[2]);
-
-  if (!isNaN(numOccurrences) && numOccurrences > 0) {
-    for (let i = 0; i < numOccurrences; i++) {
-      console.log('C is fun');
-    }
-  } else {
-    console.log('Missing number of occurrences');
-  }
+if (isNaN(process.argv[2])) {
+	console.log('Missing number of occurrences');
 } else {
-  console.log('Missing number of occurrences');
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+	  console.log('C is fun');
+  }
 }
