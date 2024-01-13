da04a9254150eb596b0051868027137c59c84eaa
youssefhammani <youssefhammani04@gmail.com>
2024-01-13 15:32:23 +0100

    script that prints “JavaScript is amazing”

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -2,5 +2,4 @@
 // Create a constant variable called myVar with the value “JavaScript is amazing”
 
 const myVar = "JavaScript is amazing";
-// Use console.log(...) to print the output
 console.log(myVar);
