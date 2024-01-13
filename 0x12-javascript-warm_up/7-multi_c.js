62fe7521e642d26b1b98dd82fef163b35a2a1183
youssefhammani <youssefhammani04@gmail.com>
2024-01-13 15:58:47 +0100

    script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
+// prints 3 lines: (like 1-multi_languages.js) but by
+// using an array of string and a loop
 
-// Check if the first argument is provided
 if (process.argv[2]) {
-  // Convert the first argument to an integer
   const numOccurrences = parseInt(process.argv[2]);
 
-  // Check if the conversion is successful
   if (!isNaN(numOccurrences) && numOccurrences > 0) {
-    // Use a loop to print "C is fun" x times
     for (let i = 0; i < numOccurrences; i++) {
       console.log('C is fun');
     }
   } else {
-    // Print an error message if the conversion fails or the number is not positive
     console.log('Missing number of occurrences');
   }
 } else {
-  // Print an error message if no argument is provided
   console.log('Missing number of occurrences');
 }
