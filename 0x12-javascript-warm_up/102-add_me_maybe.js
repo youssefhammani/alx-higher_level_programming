1eacac8ca8a0f54ef17ebfaee41dc32dfaf4ecfe
youssefhammani <youssefhammani04@gmail.com>
2024-01-13 16:42:40 +0100

    function that increments and calls a function

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Define a function named addMeMaybe that increments a number and calls another function
+exports.addMeMaybe = function (number, theFunction) {
+  number += 1; // Increment the number
+  theFunction(number); // Call the provided function with the incremented number
+}
