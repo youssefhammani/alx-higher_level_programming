bca710c8f22025324b0c2bea7e06a29b62f5c734
youssefhammani <youssefhammani04@gmail.com>
2024-01-25 19:49:51 +0100

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL and displays the body of the response for a 200 status code
-curl -sL -w "%{http_code}" "$1" -o /dev/null | grep -w "200" && curl -sL "$1"
+curl -sX GET $1 -L
