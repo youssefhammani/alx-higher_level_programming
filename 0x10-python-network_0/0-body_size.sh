bf3fae53719c5144843dee43b1385e61fbddf294
youssefhammani <youssefhammani04@gmail.com>
2024-01-25 19:29:26 +0100

    A readme file was created and terminated with Task_0 'cURL body size'

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Sends a request to the URL and displays the size of the response body in bytes
 
-curl -sI $1 | grep -i "Content-Length" | cut -d " " -f2
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
