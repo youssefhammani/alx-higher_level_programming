bb9fc9ae97b319b81304e70e96c427bf38ee91b1
youssefhammani <youssefhammani04@gmail.com>
2024-01-25 20:48:44 +0100

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!"
-curl -s -L -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me_3
+curl -s -L -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
