f9fbd2c7b6e0a1bf68c7ebcadb36b3eb42c52105
youssefhammani <youssefhammani04@gmail.com>
2024-01-26 19:49:31 +0100

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
-        print("{}".format(body))
+        print(body)
