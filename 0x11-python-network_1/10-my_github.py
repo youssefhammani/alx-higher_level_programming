3e16e34984669ca2183a7bd0610c6a8161cf1a65
youssefhammani <youssefhammani04@gmail.com>
2024-01-26 20:16:10 +0100

    My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Uses Basic Authentication with a personal access token
+to display the GitHub user id.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+
+    response = requests.get(url, auth=(username, password))
+
+    try:
+        json_response = response.json()
+        print(json_response['id'])
+    except ValueError:
+        print(None)
